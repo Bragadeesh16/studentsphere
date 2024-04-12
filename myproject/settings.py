1a0651355c25c8cc8da7070b3268d7e30f05cf7f
bragadeeshwaran <bragadeesh03294@gmail.com>
2024-04-12 22:19:43 +0530

    the bug are cleared

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,14 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-)+l-zcyxz(uln4u*)tido7q05*v3fhh!#j=$p43ajh9p83^wwv'
-DEBUG = True
-ALLOWED_HOSTS = []
+# SECRET_KEY = 'django-insecure-)+l-zcyxz(uln4u*)tido7q05*v3fhh!#j=$p43ajh9p83^wwv'
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # Application definition
 
@@ -89,8 +90,8 @@ DATABASES = {
     }
 }
 
-# database_url = os.environ.get("DATABASE_URL")
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
